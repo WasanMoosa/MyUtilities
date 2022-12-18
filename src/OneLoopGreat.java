03cc4ae75e8fce914ae8794cd06be245f9b4a2d6
WasanMoosa <wasssoon@gmail.com>
2022-12-18 14:54:35 +0400

    Done three greatest number with 1 loop

---
+++ src/OneLoopGreat.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+
+public class OneLoopGreat {
+	static Scanner sc = new Scanner(System.in);
+	static int number = 0;
+	static int[] greatNum = new int[3];
+	static int ref1=0;
+	static int ref2=0;
+	static int ref3=0;
+
+	public static void main(String[] args) {
+
+		int[] arr = {100, -1, -2, -3, 9, 7, 88, -1, 0 , 999, 8,77,-1,0, 9889, 999};
+		
+		greatNum[0] = -1999;
+		greatNum[1] = -1999;
+		greatNum[2] = -1999;
+
+		int[] greatestN = great(arr);
+
+		System.out.printf(" %d is the largest number, %d is 2nd largest number, %d is 3rd largest number.",
+				greatestN[0], greatestN[1], greatestN[2]);
+
+	}
+
+	static int[] great(int arr[]) {
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (greatNum[0] < arr[i]) {
+				greatNum[0] = arr[i];
+			}
+			if (greatNum[1] < arr[i] && greatNum[0] != arr[i] ) {
+				greatNum[1] = arr[i];
+		}
+			if (greatNum[2] < arr[i] && greatNum[0] != arr[i] && greatNum[1] != arr[i]) {
+					greatNum[2] = arr[i];
+					}
+				}
+
+
+		return greatNum;
+}
+
+}
