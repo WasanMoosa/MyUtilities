d9dc40ed3bcf752f1c74a8b32f32abd57749ab54
WasanMoosa <wasssoon@gmail.com>
2022-12-22 09:01:52 +0400

    Hash of MatchMaking Done

--- src/MatchHashMap.java
+++ src/MatchHashMap.java
@@ -1,16 +1,19 @@
+import java.io.NotActiveException;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class MatchHashMap {
 	static Integer[] couple = new Integer[2];
 	static HashMap<Integer, Integer> numbers = new HashMap<>();
-	// static Integer[] array = { 6, 7, 4, 7, 4, 3, 6, 4, 5 };
-	static Integer[] array = { 6,6,6,4,4,4 };
+	static Integer[] array = { 6, 7, 4, 7, 4, 3, 6, 4, 5 };
+	// static Integer[] array = { 6, 6, 6, 4, 4, 4 };
 	static int target = 10;
+	static boolean notStop = false;
 
 	public static void main(String[] args) {
 		System.out.println("The pairs of " + target + " from " + Arrays.toString(array) + " are");
 		for (int i = 0; i < array.length; i++) {
+			notStop = false;
 
 			int temp = target - array[i]; // To find the second pair
 
@@ -20,7 +23,11 @@ public class MatchHashMap {
 				numbers.put(array[i], 1);
 
 			// Print only if find pairs
-			if (numbers.get(temp) != null && numbers.get(temp) != 0 && numbers.get(array[i]) != 0 ) {
+			if (temp == array[i] && numbers.get(temp) < 2) {
+				notStop = true;
+			}
+
+			if (numbers.get(temp) != null && numbers.get(temp) != 0 && numbers.get(array[i]) != 0 && !notStop) {
 				numbers.put(array[i], numbers.get(array[i]) - 1); // Decrement the number of appearance of number
 				numbers.put(temp, numbers.get(temp) - 1); // Decrement the number of appearance of number
 				couple[0] = temp;
